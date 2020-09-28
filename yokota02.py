0ebc271b37aff03f5845b2c5a9052c31586dedd3
naokoyokota <52523405+naokoyokota@users.noreply.github.com>
2020-09-28 15:05:37 +0900

    Delete yokota02.py

--- yokota02.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-import argparse
-import numpy as np
-import pandas as pd
-df= pd.read_table('address.txt', encoding='utf-8',header=None)
-df.to_csv('address_space.txt', encoding='utf-8',sep=' ')
