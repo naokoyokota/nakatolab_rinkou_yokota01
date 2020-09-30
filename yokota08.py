954f58aa00fea6a7bea97a377f505b851637400a
naokoyokota <52523405+naokoyokota@users.noreply.github.com>
2020-09-30 14:39:11 +0900

    Add files via upload

---
+++ yokota08.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import argparse
+import pandas as pd
+def read_file(path):
+    lines = []
+    with open(path, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+        lines = [ln.strip(os.linesep) for ln in lines]
+
+    return lines
+
+# change this function that uses pandas library
+#def count_lines(file_path):
+#    lines = read_file(file_path)
+#    return len(lines)
+
+# main関数を定義 (to increase readability)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="show partial lines of file")
+    parser.add_argument("file", help="file path", type=str)
+    #parser.add_argument("number", help="number of lines", type=int)
+
+    args = parser.parse_args()
+
+    filename = args.file
+    #nlines = args.number
+    #countlines with pandas
+    df= pd.read_table(filename, encoding='utf-8',header=None) #topic１
+    #A=len(df) #topic1
+    #df.to_csv('address_space.txt', encoding='utf-8',sep=' ') #topic２
+    print('file: %s' % filename)
+    for index, value in df[1].value_counts().iteritems(): #topic8
+        print(index, ': ', value) #topic8
