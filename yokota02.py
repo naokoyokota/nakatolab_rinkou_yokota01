1ea08fc610d0fdb78acced467ee4184d6a373773
naokoyokota <york@iam.u-tokyo.ac.jp>
2020-10-01 13:41:45 +0900

    課題２を修正

--- yokota02.py
+++ yokota02.py
@@ -3,5 +3,22 @@ import sys
 import argparse
 import numpy as np
 import pandas as pd
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="show partial lines of file")
+    parser.add_argument("file", help="file path", type=str)
+    parser.add_argument("number", help="number of lines", type=int)
+
+    args = parser.parse_args()
+
+    filename = args.file
+    nlines = args.number
+
+    print('file: %s' % filename)
+    print('number: %d' % nlines)
+    #len = count_lines(filename)
+
+    #countlines with pandas
 df= pd.read_table('address.txt', encoding='utf-8',header=None)
 df.to_csv('address_space.txt', encoding='utf-8',sep=' ')
+
+
