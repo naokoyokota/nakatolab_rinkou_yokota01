b507986251e17f470a87a6491f9210d9b5e9464b
naokoyokota <york@iam.u-tokyo.ac.jp>
2020-10-06 15:47:36 +0900

    modified with comments

--- yokota05.py
+++ yokota05.py
@@ -11,8 +11,11 @@ def read_file(path):
     return lines
 
 # change this function that uses pandas library
-#def count_lines(file_path):
+def read_file(file_path):
 #    lines = read_file(file_path)
+    #countlines with pandas
+    df= pd.read_table(file_path, encoding='utf-8',header=None,names=["city", "town"])
+    return df
 #    return len(lines)
 
 # mainéæをåç (to increase readability)
@@ -26,6 +29,6 @@ if __name__ == "__main__":
     filename = args.file
     #nlines = args.number
     #print count of the first column removed duplicates with pandas
-    df= pd.read_table(filename, encoding='utf-8',header=None) #topic5
-    for index, value in df[0].value_counts().iteritems(): #topic5
+    df = read_file(filename)
+    for index, value in df["city"].value_counts().iteritems(): #topic5
         print(index, ': ', value) #topic5
