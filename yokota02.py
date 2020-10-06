97f2ca6e3216944bead0db51b5f8e49bb9bb727c
naokoyokota <york@iam.u-tokyo.ac.jp>
2020-10-06 15:16:26 +0900

    課題１の結果（自作関数を利用するかたち）を活かすかたちで

--- yokota02.py
+++ yokota02.py
@@ -12,30 +12,29 @@ import pandas as pd
 
 #    return lines
 
-# change this function that uses pandas library
-#def count_lines(file_path):
+#change this function that uses pandas library
+def read_file(file_path):
 #    lines = read_file(file_path)
     #countlines with pandas
-
-#    return len(df)
-
+    df= pd.read_table(file_path, encoding='utf-8',header=None,names=["city", "town"])
+    return df
 
 # main関数を定義 (to increase readability)
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="show partial lines of file")
     parser.add_argument("file", help="file path", type=str)
-    parser.add_argument("number", help="number of lines", type=int)
+    #parser.add_argument("number", help="number of lines", type=int)
 
     args = parser.parse_args()
 
     filename = args.file
-    nlines = args.number
+    #nlines = args.number
 
     print('file: %s' % filename)
-    print('number: %d' % nlines)
-#    len = count_lines(filename)
+    #print('number: %d' % nlines)
+    df = read_file(filename)
 # save as a file with space
-df= pd.read_table(filename, encoding='utf-8',header=None) #topic１
-df.to_csv('address_space.txt', encoding='utf-8',sep=' ')
+    df.to_csv('address_space.txt', encoding='utf-8',sep=' ')
+
 
 #    print('length is {}'.format(len)) #topic1
