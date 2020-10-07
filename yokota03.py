4e7ef62216c7fd162799f836338fa300def2846c
naokoyokota <york@iam.u-tokyo.ac.jp>
2020-10-07 14:26:04 +0900

    changed with review

--- yokota03.py
+++ yokota03.py
@@ -14,13 +14,17 @@ if __name__ == "__main__":
     filename = args.file
     nlines = args.number
 
-    #countlines with pandas
+    #read a file with pandas
     df = pd.read_table(filename, encoding='utf-8',header=None)
 
     print('file: %s' % filename)
     print('number: %d' % nlines)
 
-    num_lines = nlines # topic3
     print(filename)
     print(nlines)
-    print(df.head(num_lines)) #topic3
+    #extract n lines from tail and save into a file
+    df=df.tail(nlines)
+    prefix = os.path.splitext(filename)[0]
+    output3 = '{}_nlines_tail.txt'.format(prefix)
+    df.to_csv(output3, encoding='utf-8',columns=None, header=None, index=None,sep=' ')
+     #topic3
