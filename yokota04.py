ea77e90b9b35854620bbd4e8b797b9597d8bd686
naokoyokota <york@iam.u-tokyo.ac.jp>
2020-10-07 14:26:24 +0900

    changed with review

--- yokota04.py
+++ yokota04.py
@@ -2,18 +2,6 @@ import os
 import sys
 import argparse
 import pandas as pd
-def read_file(path):
-    lines = []
-    with open(path, "r", encoding="utf-8") as f:
-        lines = f.readlines()
-        lines = [ln.strip(os.linesep) for ln in lines]
-
-    return lines
-def read_file(file_path):
-#    lines = read_file(file_path)
-    #countlines with pandas
-    df= pd.read_table(file_path, encoding='utf-8',header=None,names=["city", "town"])
-    return len(df)
 
 # main関数を定義 (to increase readability)
 if __name__ == "__main__":
@@ -25,15 +13,18 @@ if __name__ == "__main__":
 
     filename = args.file
     nlines = args.number
-    #countlines with pandas
-    df= pd.read_table(filename, encoding='utf-8',header=None)
-    num=len(df) #topic1
+
+    #read a file with pandas
+    df = pd.read_table(filename, encoding='utf-8',header=None)
 
     print('file: %s' % filename)
     print('number: %d' % nlines)
-    num_lines=nlines # topic3
-    
-    #show number of lines from tail
+
     print(filename)
     print(nlines)
-    print(df.tail(num_lines)) #topic3
+    #extract n lines from tail and save into a file
+    df=df.tail(nlines)
+    prefix = os.path.splitext(filename)[0]
+    output3 = '{}_nlines_tail.txt'.format(prefix)
+    df.to_csv(output3, encoding='utf-8',columns=None, header=None, index=None,sep=' ')
+     #topic3
